5fb7ed90f1b48e2db14e7073f1c935f5587e9ef7
Tom Hulihan <hulihan.tom159@gmail.com>
2012-11-17 17:39:00 -0500

    make a skeleton job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,83 +1,81 @@
 class JobsController < ApplicationController
+  
+  before_filter :require_login, only: [:index, :new, :edit, :create, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update, :destroy]
+  
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @jobs }
-    end
+    @job = current_user.job
   end
 
   # GET /jobs/1
   # GET /jobs/1.json
   def show
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @job }
-    end
   end
 
   # GET /jobs/new
   # GET /jobs/new.json
   def new
     @job = Job.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @job }
-    end
   end
 
   # GET /jobs/1/edit
   def edit
-    @job = Job.find(params[:id])
   end
 
   # POST /jobs
   # POST /jobs.json
   def create
-    @job = Job.new(params[:job])
-
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
-        format.json { render json: @job, status: :created, location: @job }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    @job = current_user.jobs.build(params[:job])
+    if @job.save
+      flash[:success] = "Job created"
+      redirect_to @job
+    else
+      render 'new'
     end
   end
 
   # PUT /jobs/1
   # PUT /jobs/1.json
   def update
-    @job = Job.find(params[:id])
-
-    respond_to do |format|
-      if @job.update_attributes(params[:job])
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    if @job.update_attributes(params[:job])
+      flash[:success] = "Job updated"
+      redirect_to @job
+    else
+      render 'edit'
     end
   end
 
   # DELETE /jobs/1
   # DELETE /jobs/1.json
   def destroy
-    @job = Job.find(params[:id])
     @job.destroy
-
-    respond_to do |format|
-      format.html { redirect_to jobs_url }
-      format.json { head :no_content }
-    end
+    flash[:success] = "Job destroyed"
+    redirect_to users_url
   end
+
+
+  private
+    def require_login
+      unless logged_in?
+        flash[:error] = "Only logged in users can add edit their jobs"
+        redirect_to :back
+      end
+    end
+
+    def correct_seller
+      @job = current_user.jobs.find_by_id(params[:id])
+      unless @job
+        flash[:error] = "You cannot modify someone else's job"
+        redirect_to :back
+      end
+    end
+
+    # 'bool-ize' the current_seller
+    def logged_in?
+      !!current_user
+    end
+  
 end
