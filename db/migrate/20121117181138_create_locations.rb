82200fade61748fe9594fcc6b676793eb0115b23
Tom Hulihan <hulihan.tom159@gmail.com>
2012-11-17 13:12:11 -0500

    created a location scaffold

---
+++ db/migrate/20121117181138_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
