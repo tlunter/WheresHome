9eb8db8d5821d426112cd6ff9bda7324b30a810a
Todd Lunter <tlunter@gmail.com>
2012-11-17 15:41:07 -0500

    Add omniauth to buyers for LinkedIn login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,4 +229,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  require "omniauth-linkedin"
+  config.omniauth :linkedin, "n9pf2i33qnos", "F482327EvZEo7LQ7"
 end
