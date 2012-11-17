d299edf9efda2514780e877c48162698890d282a
Tom Hulihan <hulihan.tom159@gmail.com>
2012-11-17 14:39:28 -0500

    made a basic location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -13,4 +13,22 @@
 
 class Location < ActiveRecord::Base
   attr_accessible :address1, :address2, :city, :state
+  
+  before_save do |loc|
+    loc.state = loc.state.upcase
+  end
+
+  VALID_STATE_REGEX = /[a-zA-Z]{2}/
+  VALID_CITY_REGEX  = /[a-zA-Z]+/
+
+  validates :state, prescence: true,
+                    format: { with: VALID_STATE_REGEX }
+  validates :city, prescence: true,
+                   format: { with: VALID_CITY_REGEX }
+  validates :address1, presence: true
+
+  belongs_to :buyer
+  belongs_to :property
+  belongs_to :job
+
 end
